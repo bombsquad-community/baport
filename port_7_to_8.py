fd0cef0f1fd7e9b204debae445bad31c73c9fd65
ragafrawon <143259413+ragafrawon@users.noreply.github.com>
2023-08-26 13:43:50 +0300

    Update port_7_to_8.py
    Basketball match team red vs team blue
    
    For 1.7.26 api 8

--- port_7_to_8.py
+++ port_7_to_8.py
@@ -1,4 +1,4 @@
-# Usage: port_7_to_8.py <client/server type of mod> <plugin-name>
+# Usage: port_7_to_8.py <client/server team red vs team blue in basketball match> <basketball.py>
 
 # You'll have to manually update the following:
 # with ba.Context(_ba.foreground_host_activity()):
